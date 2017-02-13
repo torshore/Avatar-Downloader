e9d8d505ff25bb2c6b9e06445ba888769e1ca125
Tory Shoreman <toryshoreman@gmail.com>
2017-02-13 16:50:30 +0000

    Adding Solution to Step 1

---
+++ step1.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      console.log('Chunk Received. Length:', data.length, '\n');
+      console.log(data.toString());
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.')
+  });
+});
+};
+getAndPrintHTMLChunks();
